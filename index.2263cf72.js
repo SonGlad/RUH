5a64e511d4336caaa3b8d738637333e5821681b4
SonGlad <121556627+SonGlad@users.noreply.github.com>
2023-08-15 14:49:05 +0000

    Deploying to gh-pages from @ SonGlad/ruh@08deb92f29874daaf0451bb8c61692b89aad7381 🚀

--- index.2263cf72.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper2",{loop:!0,allowSlideNext:!0,centeredSlides:!0,centeredSlidesBounds:!0,spaceBetween:75,slideClass:"swiper-slide2",slidesPerView:2,autoplay:{delay:3e3,disableOnInteraction:!1},breakpoints:{1280:{spaceBetween:-5,slidesPerView:3}}});
-//# sourceMappingURL=index.2263cf72.js.map
