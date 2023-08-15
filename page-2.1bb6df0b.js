1ac2ac84ea11443b1dbdbd77ab924372478d2381
SonGlad <121556627+SonGlad@users.noreply.github.com>
2023-08-15 11:31:04 +0000

    Deploying to gh-pages from @ SonGlad/ruh@0c9da7022722e27e73c421c949b5f8085628aae0 🚀

--- page-2.1bb6df0b.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){const n=document.querySelector("#main-content-list"),e=Array.from(n.children),t=e.length-1;function i(){if(window.innerWidth>=390&&window.innerWidth<=767&&t%2==0){e[t].style.transform="translate(176px, 0)"}}i(),window.addEventListener("resize",i)}));
-//# sourceMappingURL=page-2.1bb6df0b.js.map
--- page-2.aa748e02.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var n=document.querySelector("#main-content-list"),e=Array.from(n.children),t=e.length-1;function i(){window.innerWidth>=390&&window.innerWidth<=767&&t%2==0&&(e[t].style.transform="translate(176px, 0)")}i(),window.addEventListener("resize",i)}));
-//# sourceMappingURL=page-2.aa748e02.js.map
